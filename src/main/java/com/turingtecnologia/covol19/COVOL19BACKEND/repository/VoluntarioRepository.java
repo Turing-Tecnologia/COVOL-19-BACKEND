b0958f9ed364236304c7200158748bebb784e89f
cesarionto <cesarionto>
2020-03-20 11:26:52 -0300

    implementação do metodo que retorna os voluntarios por cidade concluido

--- src/main/java/com/turingtecnologia/covol19/COVOL19BACKEND/repository/VoluntarioRepository.java
+++ src/main/java/com/turingtecnologia/covol19/COVOL19BACKEND/repository/VoluntarioRepository.java
@@ -9,7 +9,6 @@ import com.turingtecnologia.covol19.COVOL19BACKEND.models.Voluntario;
 
 public interface VoluntarioRepository extends JpaRepository<Voluntario, Long>{
 	
-	//@Query(value = "select * from tb_voluntarios where cidade = :cidade ", nativeQuery = true)
 	List<Voluntario> findByCidadeLikeIgnoreCase(String cidade);
 
 }
