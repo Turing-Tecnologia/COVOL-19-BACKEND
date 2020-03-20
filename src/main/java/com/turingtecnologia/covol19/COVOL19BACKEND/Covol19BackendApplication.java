e67fcedd97802504bb6a814a5d4e2479f039df00
cesarionto <cesarionto>
2020-03-20 00:52:04 -0300

    primeiro commit

---
+++ src/main/java/com/turingtecnologia/covol19/COVOL19BACKEND/Covol19BackendApplication.java
@@ -0,0 +1,13 @@
+package com.turingtecnologia.covol19.COVOL19BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covol19BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covol19BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turingtecnologia/covol19/COVOL19BACKEND/Covol19BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turingtecnologia.covol19.COVOL19BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covol19BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
